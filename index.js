dac1fd17bc3080ce8eeeecdd1ccc0d5db04558e9
Chris Wiseman <cjwiseman11@gmail.com>
2017-05-02 21:39:52 +0100

    Updated database to use real

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'hedgehog123',
+  host     : '10.169.0.144',
+  user     : 'pepperte_cwise',
+  password : '(Hedgehog123)',
   dateStrings:true,
-  database : 'gg'
+  database : 'pepperte_neighbour'
 });
 
 connection.connect();
