cd6e33bdee4520127826078cf6cab6ec23d7f867
Chris Wiseman <cjwiseman11@gmail.com>
2017-05-10 17:15:44 +0100

    Minor fixes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require('express');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
